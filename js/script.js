50452309b638d1dc3d606c00e7b4e476ca6264a4
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2021-08-18 15:39:25 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -15,3 +15,13 @@ $(document).ready(function() {
       $('body').toggleClass('fixed');
   });
 });
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        $(this).next().slideToggle()
+    });
+}
